15587e2d304257a1ec238ba88c9079e71cd89e50
Adel MALIK-MONROCQ <dedeloum@icloud.com>
2022-03-05 18:37:27 +0100

    add env conf

---
+++ bin/deploy.sh
@@ -0,0 +1,8 @@
+eb setenv BCRYPT_PASSWORD=$BCRYPT_PASSWORD
+eb setenv DB=$DB
+eb setenv SALT_ROUND=$SALT_ROUND
+eb setenv TOKEN_SECRET=$TOKEN_SECRET
+eb setenv database=$database
+eb setenv host_prod=$host_prod
+eb setenv password_prod=$password_prod
+eb setenv user=$user
